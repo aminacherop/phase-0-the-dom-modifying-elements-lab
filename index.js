f1710df9d7331d10cc500259156ed5ada04165ae
aminacherop <cheropamina26@gmail.com>
2025-03-15 16:16:46 +0300

    Dom

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Amina is the champion";
+document.body.appendChild(newHeader);
+console.log(newHeader);
